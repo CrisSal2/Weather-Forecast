0dc78b44e2da5f6c750bd52a76f576a949e86cd4
Chris_S <chris.s@apluskitchen.com>
2024-05-13 15:58:44 -0700

    added api key and other html

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+/*********************** API key ***********************/
+
+const APIkey = 'efe69132a772f9de7f8b7a79c59ed1fb';
+
+/*********************** Selectors *********************/
+
+const cityForm = document.querySelector('#city-form');
+const cityInput = document.querySelector('#city-input');
